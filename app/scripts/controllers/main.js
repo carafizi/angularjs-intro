ec9ac4f56035b20c9f61431843cc85a6f8f666f3
acarafizi <acarafizi@casenetllc.com>
2015-08-27 17:19:05 +0200

    initial yeoman project structure

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularIntroApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularIntroApp
+ */
+angular.module('angularIntroApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/user.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularIntroApp.controller:UserCtrl
+ * @description
+ * # UserCtrl
+ * Controller of the angularIntroApp
+ */
+angular.module('angularIntroApp')
+  .controller('UserCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
